6f4e9560cd526bb9a55c82af44800926e074f08f
Alok Saxena <aloksaxenagit@gmail.com>
2020-02-24 14:18:22 +0530

    Create Sorting Utility in Repository Extension

--- CS.Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ CS.Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Linq.Dynamic.Core;
+using CS.Repository.Extensions.Utility;
 
 namespace CS.Repository.Extensions
 {
@@ -30,29 +31,45 @@ namespace CS.Repository.Extensions
                 return employees.OrderBy(e => e.Name);
             }
 
-            var orderParams = orderByQueryString.Trim().ToLower().Split(',');
-            var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            var orderQueryBuilder = new StringBuilder();
-            foreach (var param in orderParams)
-            {
-                if (string.IsNullOrEmpty(param)) continue;
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
 
-
-                var propertyFromQueryName = param.Split(" ")[0];
-                var objectProperty = propertyInfos.FirstOrDefault(pi =>
-                    pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
-                if (objectProperty == null) continue;
-                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
-            }
-
-            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
             if (string.IsNullOrWhiteSpace(orderQuery))
             {
                 return employees.OrderBy(e => e.Name);
             }
-
-            return employees.OrderBy(orderQuery); // System.Linq.Dynamic.Core;
+            return employees.OrderBy(orderQuery);
         }
+
+        //public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQueryString)
+        //{
+        //    if (string.IsNullOrWhiteSpace(orderByQueryString))
+        //    {
+        //        return employees.OrderBy(e => e.Name);
+        //    }
+
+        //    var orderParams = orderByQueryString.Trim().ToLower().Split(',');
+        //    var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        //    var orderQueryBuilder = new StringBuilder();
+        //    foreach (var param in orderParams)
+        //    {
+        //        if (string.IsNullOrEmpty(param)) continue;
+
+
+        //        var propertyFromQueryName = param.Split(" ")[0];
+        //        var objectProperty = propertyInfos.FirstOrDefault(pi =>
+        //            pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+        //        if (objectProperty == null) continue;
+        //        var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+        //        orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
+        //    }
+
+        //    var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+        //    if (string.IsNullOrWhiteSpace(orderQuery))
+        //    {
+        //        return employees.OrderBy(e => e.Name);
+        //    }
+
+        //    return employees.OrderBy(orderQuery); // System.Linq.Dynamic.Core;
+        //}
     }
 }
